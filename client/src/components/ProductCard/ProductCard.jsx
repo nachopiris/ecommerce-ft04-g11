8104cdd02776895705a10ef486ca97bbe6a7478e
alantoledo007 <alantoledo007@gmail.com>
2020-09-04 01:26:39 -0300

    Number format, icons & product card styles

--- client/src/components/ProductCard/ProductCard.jsx
+++ client/src/components/ProductCard/ProductCard.jsx
@@ -1,26 +1,36 @@
 import React from 'react';
 import s from './productCard.module.scss';
 import {Card, Col, Button, Badge} from 'react-bootstrap';
+import { CgShoppingCart } from 'react-icons/cg';
+import NumberFormat from 'react-number-format';
 
 export default function ProductCard({name,price,img}) {
+    const MAX_NAME_LENGTH = 40
+    if(name.length > MAX_NAME_LENGTH){
+        name = name.substring(0,MAX_NAME_LENGTH - 3) + '...';
+    }
     return (
-        <Col md="3" className="mb-4">
-            <Card className={'h-100 bg-dark shadow ' + s.card}>
-                
-                <div className={'rounded ' + s['cover-image']}>
-                    <img
-                        src={img} alt=""
-                        title=""
-                    />
-                </div>
-                <Card.Body>
-                    <p className="mb-0 text-uppercase">{name}</p>
-                </Card.Body>
-                <Card.Footer className="border-0 bg-transparent text-right">
-                    <span className="lead font-weight-bold text-success">${price}</span>
-                    <Button variant="primary" size="lg" block>Agregar al carrito</Button>
-                </Card.Footer>
-            </Card>
+        <Col xl="2" md="3" sm="4" xs="6" className="mb-4">
+            <div className={s.card}>
+                <Card className={'border-0 bg-dark rounded-0 ' + s['card-bootstrap']}>
+                    
+                    <a href="#" className={s['cover-image']}>
+                        <img
+                            src={img} alt=""
+                            title=""
+                        />
+                    </a>
+                    <Card.Body className={s['card-bootstrap-body']}>
+                        <p className={'mb-0 text-uppercase ' + s['card-title']}>{name}</p>
+                    </Card.Body>
+                    <Card.Footer className="border-0 d-flex align-items-center py-1 bg-dark2 px-1 justify-content-between">
+                            <span className={s.price + ' ml-2'}>
+                                <NumberFormat prefix="$" value={price} decimalScale={2} fixedDecimalScale={true} displayType={'text'} decimalSeparator={','} thousandSeparator={'.'} />
+                            </span>
+                            <Button variant="outline-light border-0" size="sm"><CgShoppingCart /></Button>
+                    </Card.Footer>
+                </Card>
+            </div>
         </Col>
     );
 }
