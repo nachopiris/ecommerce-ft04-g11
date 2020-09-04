48f10f9a0d7175056b94189fec04a9fe79ca8367
Ignacio Piris <ignaciopirisb@gmail.com>
2020-09-04 11:50:27 -0300

    ruta para agregar producto

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -4,9 +4,25 @@ const { Product } = require('../db.js');
 server.get('/', (req, res, next) => {
 	Product.findAll()
 		.then(products => {
-			res.send(products);
+			res.send({data: products});
 		})
 		.catch(next);
 });
+
+server.post('/', (req, res) => {
+	Product.create({
+		name: req.body.name,
+		description: req.body.description,
+		stock: req.body.stock,
+		price: req.body.price,
+		images: JSON.stringify(req.body.images)
+	})
+	.then(() => {
+		res.status(201).send('producto creado con exito')
+	})
+	.catch(() => {
+		res.status(401).send('hubo un error')
+	})
+})
 
 module.exports = server;
