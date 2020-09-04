058d13476d7d12d4be775175bb3a311114f52ade
BryanCPineda <b.camilo.15@gmail.com>
2020-09-04 10:41:46 -0300

    created, returned products by keyword search

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,8 +1,8 @@
 const { Router } = require('express');
+const { Product } = require('../db.js');
 // import all routers;
 const productRouter = require('./product.js');
 const categoriesRouter = require('./categories.js');
-
 
 const router = Router();
 
@@ -11,5 +11,24 @@ const router = Router();
 // router.use('/auth', authRouter);
 router.use('/products', productRouter);
 router.use('/categories', categoriesRouter);
+
+router.get('/search', (req,res)=>{
+
+    const query = req.query.query;
+    let filter = []
+
+    function filtrar (product){
+         product.map((e,i)=>{
+            if(e.dataValues.name.indexOf(query)!==-1 || e.dataValues.description.indexOf(query)!==-1){
+                filter.push(e.dataValues)
+            }
+         }) 
+    }
+    Product.findAll().then((products) => {
+             filtrar(products);
+    }).then(() => {
+        res.send(filter);
+    }) 
+})
 
 module.exports = router;
