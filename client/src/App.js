91f4e262f6e662913a4d695a67ce63b387beb3c6
Alan Toledo <alantoledo007@gmail.com>
2020-09-05 18:57:37 -0300

    Route to product component by id & component product modified to get data from server (#29)
    Co-authored-by: Anibal Ignacio Çaldumbide <66750275+Caldum@users.noreply.github.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
 import './styles/ecommerce.scss';
-import 'bootstrap/dist/css/bootstrap.min.css'
-
+import {Router, BrowserRouter, Route} from 'react-router-dom'
+import Product from './components/Product/Product';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Catalogue from './components/Catalogue';
 
 function App() {
   return (
-    <div className="container">
-      <Route path="/catalogo" component={Catalogue} />
-    </div>
+    <React.Fragment>
+        <BrowserRouter>
+          <Route path="/products/:id" component={Product} />
+          <Route path="/catalogo" component={Catalogue} />
+        </BrowserRouter>
+    </React.Fragment>
   );
 }
 
