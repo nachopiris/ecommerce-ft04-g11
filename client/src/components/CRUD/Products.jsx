54fd741fcdc071535895cea43e27f643bc3ee09b
unknown <veronica.jujuy@gmail.com>
2020-09-06 07:49:20 -0300

    crud funcionando

--- client/src/components/CRUD/Products.jsx
+++ client/src/components/CRUD/Products.jsx
@@ -3,21 +3,24 @@ import React, { Component } from 'react';
 class Products extends Component{
 
     handleUpdate = () => {
-         console.log('props--------',this.props);
-        this.props.uploadEdit(this.indexNum, this.name.value, this.description.value, this.stock.value, this.price.value);
+        this.props.uploadEdit(this.indexNum, this.name.value, this.description.value, this.stock.value, this.price.value, this.image.value);
     }
+
 
 render() {
     const {allProduct, pressEditBtn, pressDelete} = this.props;
+    
     const productList = allProduct.map((product, index) =>{
+
         return product.isEditing === true ? (
             <tr key = {index}>
                 <td><input type="text" ref={(val) => {this.name=val}} required defaultValue={product.name}/></td>
                 <td><input type="text" ref={(val) => {this.description=val}} required defaultValue={product.description}/></td>
                 <td><input type="text" ref={(val) => {this.stock=val}} required defaultValue={product.stock}/></td>
                 <td><input type="text" ref={(val) => {this.price=val}} required defaultValue={product.price}/></td>
+                <td><input type="text" ref={(val) => {this.image=val}} required defaultValue={product.image}/></td>
                 <td>
-                <input type="button" value="Update" onClick={this.handleUpdate} ref={() => {this.indexNum = index}} />
+                <input type="button" value="Update" onClick={this.handleUpdate} ref={() => {this.indexNum = product.id}} />
                 </td>
             </tr>
         ) : (
@@ -26,12 +29,13 @@ render() {
                     <td>{product.description}</td>
                     <td>{product.stock}</td>
                     <td>{product.price}</td>
-                    
-                    <td><button onClick={() => pressEditBtn(index)}>Edit</button>  |  <button onClick={()=>pressDelete(index)}>Delete</button></td>
+                 
+                    {product.image && (
+                        <td> <img  src={product.image[0]} alt="" title=""  width="60" height="80"/> </td>
+                    )} 
+                    <td><button onClick={() => pressEditBtn(index)}>Edit</button>  |  <button onClick={()=>pressDelete(product.id, index)}>Delete</button></td>
             </tr>
-
-
-        )
+            )
     }
     );
 
@@ -41,8 +45,9 @@ render() {
             <tr>
                     <th>Title</th>
                     <th>Description</th>
-                    <th>Price</th>
                     <th>Stock</th>
+                    <th>Price</th>
+                    <th>Image</th>
                     <th>Action</th>
                     </tr>
             </thead>
--- client/src/components/CRUD/formCRUD.jsx
+++ client/src/components/CRUD/formCRUD.jsx
@@ -6,77 +6,63 @@ import AddProduct from './AddProduct';
 class FormCRUD extends Component{
 
     state = {
-        
         product : [],
         visible: false
     }
 
 
 getItems(){
-  console.log('aqui');
+
      fetch('http://localhost:3001/products')    // modificar cuando tengamos las rutas creadas
       .then(response => response.json())
       .then((recurso) => {
-        console.log('recurso', recurso);
-
+    
         if(recurso !== undefined){
-            const prod = recurso.map( function(e) {
-                return  { "id": e.id,
-                          "name": e.name,
-                          "description": e.description,
-                          "price": e.price,
-                          "stock": e.stock,
-                          "imagen": e.imagen,
-                          "isEditing":false
-                            }
-           });
-           console.log('prod', prod);
-
+            const prod = recurso.data.map( function(e) {
+              var image = JSON.parse(e.images);
+              return  { "id": e.id,
+                        "name": e.name,
+                        "description": e.description,
+                        "price": e.price,
+                        "stock": e.stock,
+                        "image": image,
+                        "isEditing":false
+                          }
+              });
+           
            this.setState({
             product: prod
           });
-        console.log('state', this.state);
+        
       } 
       })
       .catch(err => console.log(err))
       }
-    //   // ----------
-    //   const requestOptions = {
-    //     method: 'POST',
-    //     headers: { 'Content-Type': 'application/json' },
-    //     body: JSON.stringify({ title: 'React POST Request Example' })
-    // };
-    // fetch('https://jsonplaceholder.typicode.com/posts', requestOptions)
-    //     .then(response => response.json())
-    //     .then(data => this.setState({ postId: data.id }));
 
-addProduct = e => {
-        console.log('add product', e);
-        fetch('http://localhost:3001/products', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({
-            name : e.name,
-            description : e.description,
-            stock: 12,
-            price : 10,
-            images: ["https://url.com/example.jpg", "https://url.com/example.jpg"]
-          })
+    addProduct = e => {
+      
+      fetch('http://localhost:3001/products', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          name : e.name,
+          description : e.description,
+          stock: JSON.parse(e.stock),
+          price : JSON.parse(e.price),
+          images:[e.image]       
         })
-          .then(response => {
-            console.log(response);
-            response.json()})
-          .then(item => {
-            console.log('item----',item);
-            if(Array.isArray(item)) {
-              this.addItemToState(e);
-              
-            } else {
-              console.log('failure')
-            }
-          })
-          .catch(err => console.log(err))
-      } 
+      })
+        .then(response => {
+          if (response.status === 201){
+            this.addItemToState(e) 
+           
+          }
+          else {
+            console.log('failure')
+          }
+        })
+        .catch(err => console.log(err))
+    }
       
     
 addItemToState = (newProduct) => {
@@ -84,32 +70,36 @@ addItemToState = (newProduct) => {
     this.setState({
         product
     });
+    this.handleCLick();
 }
 
-uploadEdit = e => {
-    fetch('http://localhost:3000/crud', {
-      method: 'put',
+uploadEdit = (i, name, description, stock, price, images) => {
+  const imagen = images.split(',');
+  const modif = {
+    name : name,
+    description : description,
+    stock: JSON.parse(stock),
+    price : JSON.parse(price),
+    images: imagen
+  }
+  
+    fetch('http://localhost:3001/products/'+i, {
+      method: 'PUT',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({
-        name : this.state.product.name,
-        description : this.state.product.description,
-        price : this.state.product.price
-      })
+      body: JSON.stringify(modif)
     })
-      .then(response => response.json())
-      .then(item => {
-        if(Array.isArray(item)) {
-          // console.log(item[0])
-          this.updateState(item[0])
-         
-        } else {
-          console.log('failure')
-        }
-      })
-      .catch(err => console.log(err))
-  }
+    .then(response => {
+      if (response.status === 200){
+        this.addItemToState(modif) 
+      }
+      else {
+        console.log('failure')
+      }
+    })
+    .catch(err => console.log(err))
+}
 
 pressEditBtn = (i) => {
     let product = this.state.product;
@@ -133,38 +123,39 @@ updateState = (i, name, description, stock, price) => {
     });
 }
 
-deleteItemFromState = (i) => {
-
-    let product = this.state.product.filter((u, index) => {
-        return index !==i
-    });
-    this.setState({
-      product: product
-    });
-    
-}
-
-pressDelete = (i) => {
+pressDelete = (id,index) => {
     let confirmDelete = window.confirm('Delete item?')
-    if(confirmDelete){
-      fetch('http://localhost:3001/ruta', {
+    
+   if(confirmDelete){
+      fetch('http://localhost:3001/products/'+id, {
       method: 'delete',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({
-        i
-      })
     })
-      .then(response => response.json())
-      .then(item => {
-        this.deleteItemFromState(i)
-      })
-      .catch(err => console.log(err))
+    .then(response => {
+      console.log(response);
+      if (response.status === 200){
+        this.deleteItemFromState(index);  
+
+      }
+      else {
+        console.log('failure')
+      }
+    })
+    .catch(err => console.log(err))
     }
 }
 
+deleteItemFromState = (id) => {
 
+  let product = this.state.product.filter(index => index == id)
+  this.setState({
+    product
+  });
+  this.handleCLick();
+
+}
 
 handleCLick = () => {
     this.getItems();
@@ -178,15 +169,16 @@ listarProductos(){
     if (this.state.visible){
         return (
         <div>
-        <Products allProduct = {this.state.product} pressEditBtn={this.pressEditBtn} uploadEdit={this.uploadEdit} pressDelete={this.pressDelete} />
         <AddProduct addProduct={this.addProduct}/>
+        <Products allProduct = {this.state.product} pressEditBtn={this.pressEditBtn} uploadEdit={this.uploadEdit} pressDelete={this.pressDelete} />
+         
         </div>
         )}
 
 }
 
 render(){
-  console.log(this.state.product);
+
     return(
         <div>
         <h2>Product List</h2>
