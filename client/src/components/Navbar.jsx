1bca284c27f34c3d835062d2bc1262c2bfb07925
alantoledo007 <alantoledo007@gmail.com>
2020-09-10 12:51:07 -0300

    Navbar: scroll & toggle effect

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import style from "../styles/navbar.module.scss";
+import React, { useState, useEffect, setState } from "react";
+import { NavLink, useLocation } from "react-router-dom";
+import s from "../styles/navbar.module.scss";
 import { Navbar as Navb, Nav, Container } from "react-bootstrap";
 import config from "../config";
 import { useSpring, animated } from "react-spring";
@@ -8,97 +8,110 @@ import { useSpring, animated } from "react-spring";
 const APP_NAME = config.app.name;
 
 export default function Navbar() {
-  const props = useSpring({
-    from: {
-      opacity: 0,
-    },
-    immediate: false,
-    to: {
-      opacity: 1,
-    },
-    y: 400,
-    onFrame: (props) => {
-      window.scroll(200, props.y);
-    },
-    config: config.slow,
+  const location = useLocation();
+  const [state, setState] = useState({
+    navbarBg: "",
+    navbarExpanded: false,
+  });
+
+  const handleScroll = (e) => {
+    if (
+      (document.documentElement.scrollTop >= 50 && state.navbarBg === "") ||
+      state.navbarExpanded
+    ) {
+      setState({
+        ...state,
+        navbarBg: "navbar-colorated",
+      });
+    }
+    if (
+      document.documentElement.scrollTop < 50 &&
+      state.navbarBg !== "" &&
+      !state.navbarExpanded
+    ) {
+      setState({
+        ...state,
+        navbarBg: "",
+      });
+    }
+  };
+
+  const handleToggle = () => {
+    if (state.navbarExpanded) {
+      if (document.documentElement.scrollTop >= 50) {
+        setState({
+          ...state,
+          navbarExpanded: false,
+        });
+      } else {
+        setState({
+          ...state,
+          navbarExpanded: false,
+          navbarBg: "",
+        });
+      }
+    } else {
+      setState({
+        ...state,
+        navbarExpanded: true,
+        navbarBg: "navbar-colorated",
+      });
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   });
 
   return (
-    <animated.div style={props}>
-      <Navb expand="md" bg="transparent" variant="dark" fixed="top">
-        <Container>
-          <Navb.Brand href="#home">{APP_NAME}</Navb.Brand>
-          <Navb.Toggle aria-controls="basic-navbar-nav" />
-          <Navb.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto">
-              <NavLink
-                className="nav-link"
-                activeClassName="active"
-                exact
-                to="/"
-              >
-                Inicio
-              </NavLink>
-              <NavLink
-                className="nav-link"
-                activeClassName="active"
-                to="/catalogo"
-              >
-                Catálogo
-              </NavLink>
-              <NavLink
-                className="nav-link"
-                activeClassName="active"
-                to="/admin/productos"
-              >
-                Administrar productos
-              </NavLink>
-              <NavLink
-                className="nav-link"
-                activeClassName="active"
-                to="/admin/categorias"
-              >
-                Administrar categorías
-              </NavLink>
-            </Nav>
-          </Navb.Collapse>
-        </Container>
-      </Navb>
-
-      {/* <nav className={style.navbar}>
-        <div className={style.logo}>
-          <NavLink
-            activeClassName={style.active}
-            style={{ background: "none" }}
-            exact
-            to="/"
-          >
-            <img src="/images/logo.svg" alt="" />
-          </NavLink>
-        </div>
-        <ul>
-          <li>
-            <NavLink activeClassName={style.active} exact to="/">
+    <Navb
+      expand="md"
+      variant="dark"
+      fixed="top"
+      bg=""
+      className={
+        "mb-4 " +
+        (location.pathname === "/" ? s.navbar : s["navbar-nohome"]) +
+        " " +
+        s[state.navbarBg]
+      }
+    >
+      <Container>
+        {console.log(document.documentElement.scrollTop)}
+        <Navb.Brand href="#home">{APP_NAME}</Navb.Brand>
+        <Navb.Toggle onClick={handleToggle} aria-controls="basic-navbar-nav" />
+        <Navb.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <NavLink className="nav-link" activeClassName="active" exact to="/">
               Inicio
             </NavLink>
-          </li>
-          <li>
-            <NavLink activeClassName={style.active} to="/catalogo">
+            <NavLink
+              className="nav-link"
+              activeClassName="active"
+              to="/catalogo"
+            >
               Catálogo
             </NavLink>
-          </li>
-          <li>
-            <NavLink activeClassName={style.active} to="/admin/productos">
+            <NavLink
+              className="nav-link"
+              activeClassName="active"
+              to="/admin/productos"
+            >
               Administrar productos
             </NavLink>
-          </li>
-          <li>
-            <NavLink activeClassName={style.active} to="/admin/categorias">
+            <NavLink
+              className="nav-link"
+              activeClassName="active"
+              to="/admin/categorias"
+            >
               Administrar categorías
             </NavLink>
-          </li>
-        </ul>
-      </nav> */}
-    </animated.div>
+          </Nav>
+        </Navb.Collapse>
+      </Container>
+    </Navb>
   );
 }
