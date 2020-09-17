79eef93e4576b56ff99f370da7818556dbcdae21
Ignacio Piris <66640666+nachopiris@users.noreply.github.com>
2020-09-17 01:11:44 -0300

    ruta logout y ruta para consultar id de usuario logeado (#93)
    * ruta logout y ruta para consultar id de usuario logeado
    
    * fix

--- api/src/routes/auth.js
+++ api/src/routes/auth.js
@@ -1,48 +1,89 @@
 const server = require("express").Router();
 const passport = require("passport");
+const { User } = require("../db.js");
 
-// ruta de login, devuelve el id del usuario logeado
+server.post("/promote/:id", (req, res) => {
+  const id = req.params.id;
+  if (!Number.isInteger(id * 1)) {
+    return res
+      .status(400)
+      .send({ errors: [{ message: "La consulta no es válida" }], status: 400 });
+  }
+  User.findByPk(id)
+    .then((user) => {
+      if (!user) {
+        return res
+          .status(404)
+          .send({
+            errors: [{ message: "No existe el usuario " + id }],
+            status: 404,
+          });
+      }
+      user.role = "admin";
+      user
+        .save()
+        .then(() => {
+          res
+            .status(200)
+            .send("Se le asignó el rol de Administrador al usuario " + id);
+        })
+        .catch((err) => {
+          res
+            .status(500)
+            .send({
+              errors: [
+                {
+                  message:
+                    "Ha ocurrido un error al intentar contactar con el servidor",
+                },
+              ],
+              status: 500,
+            });
+        });
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .send({
+          errors: [
+            {
+              message:
+                "Ha ocurrido un error al intentar contactar con el servidor",
+            },
+          ],
+          status: 500,
+        });
+    });
+});
 
 server.post("/login", (req, res, next) => {
-  passport.authenticate("local", (err, user) => {
+  passport.authenticate("local", (err, user, info) => {
     if (err) {
       throw err;
     }
     if (!user) {
-      res.send("Contrasena incorrecta o usuario inexistente");
+      res.send(info).status(503);
     } else {
       req.logIn(user, (err) => {
         if (err) {
           throw err;
         }
-        res.send({ userId: req.user.id });
+        res.send("successfully authenticated");
       });
     }
   })(req, res, next);
 });
 
-server.post('/promote/:id', (req, res) => {
-    const id = req.params.id;
-    if (!Number.isInteger(id * 1)) {
-        return res.status(400).send({ errors: [{ message: 'La consulta no es válida' }], status: 400 });
-    }
-    User.findByPk(id)
-        .then(user => {
-            if (!user) {
-                return res.status(404).send({ errors: [{ message: 'No existe el usuario ' + id }], status: 404 });
-            }
-            user.role = "admin";
-            user.save()
-                .then(() => {
-                    res.status(200).send('Se le asignó el rol de Administrador al usuario ' + id);
-                })
-                .catch((err) => {
-                    res.status(500).send({ errors: [{ message: 'Ha ocurrido un error al intentar contactar con el servidor' }], status: 500 });
-                })
-        })
-        .catch(() => {
-            res.status(500).send({ errors: [{ message: 'Ha ocurrido un error al intentar contactar con el servidor' }], status: 500 });
-        })
+server.get("/logout", (req, res) => {
+  req.logOut();
+  res.send("successfully logged out");
+});
+
+server.get("/me", (req, res) => {
+  if (!req.user) {
+    return res.send("no user logged in").status(404);
+  }
+  res.json({ userId: req.user.id });
 });
 
 module.exports = server;
