ea55efcc9e2831cd99230f164095728f49a096b5
Caldum <nachocaldumbide@gmail.com>
2020-09-16 22:12:51 -0300

    adds changes & fixes

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -14,224 +14,224 @@ import { FiCheck } from 'react-icons/fi';
 const APP_NAME = config.app.name;
 
 function TextIntro() {
-  const props = useSpring({
-    from: {
-      opacity: 0,
-    },
-    to: {
-      opacity: 1,
-    },
+    const props = useSpring({
+        from: {
+            opacity: 0,
+        },
+        to: {
+            opacity: 1,
+        },
 
-    delay: 200,
+        delay: 200,
 
-    config: {
-      duration: 800,
-    },
-  });
-  return (
-    <animated.p style={props}>
-      <em>"Soy lo que los dioses han hecho de mi"</em> - Kratos
-    </animated.p>
-  );
+        config: {
+            duration: 800,
+        },
+    });
+    return (
+        <animated.p style={props}>
+            <em>"Soy lo que los dioses han hecho de mi"</em> - Kratos
+        </animated.p>
+    );
 }
 
 function ShowAppName() {
-  const props = useSpring({
-    from: {
-      opacity: 0,
-    },
-    to: {
-      opacity: 1,
-    },
-    delay: 100,
-    config: {
-      duration: 800,
-    },
-  });
-  const propsWhite = useSpring({
-    from: {
-      textShadow: "0px 0px 0px transparent",
-    },
-    to: {
-      textShadow: "0px 0px 10px white",
-    },
-    delay: 300,
-    config: {
-      duration: 1500,
-    },
-  });
-  const propsRed = useSpring({
-    from: {
-      textShadow: "0px 0px 0px transparent",
-    },
-    to: {
-      textShadow: "0px 0px 10px red",
-    },
-    delay: 300,
-    config: {
-      duration: 1500,
-    },
-  });
-  return (
-    <animated.h1 className="lg" style={props}>
-      <animated.span style={propsWhite}>Origin</animated.span>
-      <animated.span style={propsRed} className="text-redGamer">
-        Master
+    const props = useSpring({
+        from: {
+            opacity: 0,
+        },
+        to: {
+            opacity: 1,
+        },
+        delay: 100,
+        config: {
+            duration: 800,
+        },
+    });
+    const propsWhite = useSpring({
+        from: {
+            textShadow: "0px 0px 0px transparent",
+        },
+        to: {
+            textShadow: "0px 0px 10px white",
+        },
+        delay: 300,
+        config: {
+            duration: 1500,
+        },
+    });
+    const propsRed = useSpring({
+        from: {
+            textShadow: "0px 0px 0px transparent",
+        },
+        to: {
+            textShadow: "0px 0px 10px red",
+        },
+        delay: 300,
+        config: {
+            duration: 1500,
+        },
+    });
+    return (
+        <animated.h1 className="lg" style={props}>
+            <animated.span style={propsWhite}>Origin</animated.span>
+            <animated.span style={propsRed} className="text-redGamer">
+                Master
       </animated.span>
-    </animated.h1>
-  );
+        </animated.h1>
+    );
 }
 
 function ExploreButton() {
-  const props = useSpring({
-    from: {
-      opacity: 0,
-      transform: "scale(0.5)",
-    },
-    to: {
-      opacity: 1,
-      transform: "scale(1)",
-    },
+    const props = useSpring({
+        from: {
+            opacity: 0,
+            transform: "scale(0.5)",
+        },
+        to: {
+            opacity: 1,
+            transform: "scale(1)",
+        },
 
-    delay: 500,
-    config: {
-      duration: 600,
-    },
-  });
-  return (
-    <animated.div style={props}>
-      <Link
-        to="/catalogo"
-        className={s["kewx"] + " btn btn-primary shadow btn-lg"}
-      >
-        Catálogo completo
+        delay: 500,
+        config: {
+            duration: 600,
+        },
+    });
+    return (
+        <animated.div style={props}>
+            <Link
+                to="/catalogo"
+                className={s["kewx"] + " btn btn-primary shadow btn-lg"}
+            >
+                Catálogo completo
       </Link>
-    </animated.div>
-  );
+        </animated.div>
+    );
 }
 
 function HomePage({ products, getLatests, setProductToCart, getUserCart, userCart }) {
-  const props = useSpring({
-    from: {
-      filter: "brightness(1.5)",
-    },
-    to: {
-      filter: "brightness(0.65)",
-    },
+    const props = useSpring({
+        from: {
+            filter: "brightness(1.5)",
+        },
+        to: {
+            filter: "brightness(0.65)",
+        },
 
-    config: {
-      duration: 1200,
-    },
-  });
+        config: {
+            duration: 1200,
+        },
+    });
 
-  useEffect(() => {
-    getLatests();
-    getUserCart();
-  }, []);
+    useEffect(() => {
+        getLatests();
+        getUserCart();
+    }, []);
 
-  return (
-    <Container fluid>
-      <Row className={s["main-section"] + " " + s["section"]}>
-        <animated.div
-          style={props}
-          className={"overflow-hidden " + s["cover-main"]}
-        >
-          <img src="https://images.hdqwalls.com/wallpapers/thanos-vs-kratos-jt.jpg" />
-        </animated.div>
-        <div className={s["main-inside"]}>
-          <ShowAppName />
-          <TextIntro />
-          <ExploreButton />
-        </div>
-      </Row>
+    return (
+        <Container fluid>
+            <Row className={s["main-section"] + " " + s["section"]}>
+                <animated.div
+                    style={props}
+                    className={"overflow-hidden " + s["cover-main"]}
+                >
+                    <img src="https://images.hdqwalls.com/wallpapers/thanos-vs-kratos-jt.jpg" />
+                </animated.div>
+                <div className={s["main-inside"]}>
+                    <ShowAppName />
+                    <TextIntro />
+                    <ExploreButton />
+                </div>
+            </Row>
 
-      {products.rows && <Gallery setToCart={setProductToCart} userCart={userCart} products={products.rows} />}
-    </Container>
-  );
+            {products.rows && <Gallery setToCart={setProductToCart} userCart={userCart} products={products.rows} />}
+        </Container>
+    );
 }
 
 function Gallery({ products, userCart, setToCart }) {
-  const handleOnDragStart = (e) => e.preventDefault();
-  useEffect(() => { }, []);
+    const handleOnDragStart = (e) => e.preventDefault();
+    useEffect(() => { }, []);
 
-  const isAdded = (id) => {
-    return userCart.find(item => item.productId === id) ? true : false;
-  }
-  var items = products
-    .map((item, index) => (
-      <div
-        key={index}
-        className={"text-decoration-none text-light " + s["carousel-item"]}
-      >
-        {console.log(item)}
-        <div className={s["cover-carousel"]}>
-          <img
-            src={JSON.parse(item.images)[1] || JSON.parse(item.images)[0]}
-            className={s["perspective"]}
-          />
-        </div>
-        <span className="mb-4 mt-auto">{item.name}</span>
-        <span className="mt-auto mb-4">
-          {!isAdded(item.id) && <Button onClick={() => setToCart(item.id, 1)} variant="light">
-            <CgShoppingCart />
-          </Button>}
+    const isAdded = (id) => {
+        return userCart.find(item => item.productId === id) ? true : false;
+    }
+    var items = products
+        .map((item, index) => (
+            <div
+                key={index}
+                className={"text-decoration-none text-light " + s["carousel-item"]}
+            >
+                {console.log(item)}
+                <div className={s["cover-carousel"]}>
+                    <img
+                        src={JSON.parse(item.images)[1] || JSON.parse(item.images)[0]}
+                        className={s["perspective"]}
+                    />
+                </div>
+                <span className="mb-4 mt-auto">{item.name}</span>
+                <span className="mt-auto mb-4">
+                    {!isAdded(item.id) && <Button onClick={() => setToCart(item.id, 1)} variant="light">
+                        <CgShoppingCart />
+                    </Button>}
 
-          {isAdded(item.id) && <Link to="/carrito" className="btn btn-light border-0">
-            <span className="text-primary"><FiCheck /></span>
-            <CgShoppingCart />
-          </Link>}
-          <Link
-            to={"/productos/" + item.id}
-            className="ml-1 btn btn-outline-light"
-          >
-            Detalles
+                    {isAdded(item.id) && <Link to="/carrito" className="btn btn-light border-0">
+                        <span className="text-primary"><FiCheck /></span>
+                        <CgShoppingCart />
+                    </Link>}
+                    <Link
+                        to={"/productos/" + item.id}
+                        className="ml-1 btn btn-outline-light"
+                    >
+                        Detalles
           </Link>
-        </span>
-      </div>
-    ))
-    .concat(
-      <div className={s["carousel-item"]}>
-        <p className="mb-0">
-          <h2 className={s["kewx"]}>Explorá</h2>
-        </p>
-        <p className={s.kewx}>Mirá nuestro catálogo completo!</p>
-        <Link
-          to="/catalogo"
-          className={s["kewx"] + " px-5 btn btn-outline-primary mb-4 btn-lg"}
-        >
-          Ver todos
+                </span>
+            </div>
+        ))
+        .concat(
+            <div className={s["carousel-item"]}>
+                <h2 className="mb-0">
+                    <p className={s["kewx"]}>Explorá</p>
+                </h2>
+                <p className={s.kewx}>Mirá nuestro catálogo completo!</p>
+                <Link
+                    to="/catalogo"
+                    className={s["kewx"] + " px-5 btn btn-outline-primary mb-4 btn-lg"}
+                >
+                    Ver todos
         </Link>
-      </div>
-    );
+            </div>
+        );
 
-  return (
-    <Row className={s["section2"]}>
-      <AliceCarousel
-        mouseTrackingEnabled
-        mouseDragEnabled
-        items={items}
-        infinite={false}
-        responsive={{ 0: { items: 2 }, 500: { items: 3 }, 1024: { tems: 6 } }}
-        dotsDisabled
-        buttonsDisabled
-      />
-    </Row>
-  );
+    return (
+        <Row className={s["section2"]}>
+            <AliceCarousel
+                mouseTrackingEnabled
+                mouseDragEnabled
+                items={items}
+                infinite={false}
+                responsive={{ 0: { items: 2 }, 500: { items: 3 }, 1024: { tems: 6 } }}
+                dotsDisabled
+                buttonsDisabled
+            />
+        </Row>
+    );
 }
 
 function mapStateToProps(state) {
-  return {
-    products: state.productsReducer.products,
-    userCart: state.usersReducer.userCart
-  };
+    return {
+        products: state.productsReducer.products,
+        userCart: state.usersReducer.userCart
+    };
 }
 
 function mapDispatchToProps(dispatch) {
-  return {
-    getLatests: () => dispatch(getLatests()),
-    setProductToCart: (productId, quantity) => dispatch(setProductToCart(productId, quantity)),
-    getUserCart: () => dispatch(getUserCart())
-  };
+    return {
+        getLatests: () => dispatch(getLatests()),
+        setProductToCart: (productId, quantity) => dispatch(setProductToCart(productId, quantity)),
+        getUserCart: () => dispatch(getUserCart())
+    };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -3,6 +3,9 @@ import { login } from '../actions/users';
 import { useForm } from "react-hook-form";
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
+import { IconContext } from 'react-icons';
+import { BiShowAlt, BiHide } from 'react-icons/bi';
+import { Link } from 'react-router-dom';
 
 function Login({ login }) {
     const { register, handleSubmit, watch, errors } = useForm();
@@ -10,7 +13,7 @@ function Login({ login }) {
     console.log(watch("email"));
 
     const regValidated = {
-        email: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+        email: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/
     }
 
     const [state, setState] = useState({
@@ -29,41 +32,46 @@ function Login({ login }) {
             <Row className="justify-content-center">
                 <Col md={8}>
                     <Card className="border-0 bg-dark2">
-                        <Card.Header className="border-0">
-                            Iniciar sesión
-                                                </Card.Header>
+                        <Card.Header className="border-0 h3">
+                            Ingreso
+                        </Card.Header>
                         <Card.Body>
                             <Form onSubmit={handleSubmit(onSubmit)}>
                                 <Form.Group>
                                     <Form.Label>Correo electrónico</Form.Label>
-                                    <Form.Control autoComplete="off" className={errors.email ? 'is-invalid' : ''} ref={register({ required: true, pattern: regValidated.email, maxLength: 191 })} type="text" name="email" ></Form.Control>
+                                    <Form.Control autoComplete="off" className={errors.email ? 'is-invalid' : 'mail-input'} ref={register({ required: true, pattern: regValidated.email, maxLength: 191 })} type="text" name="email" ></Form.Control>
                                     <Form.Control.Feedback type="invalid">
                                         {errors.email?.type === "required" &&
-                                            "Se requiere un correo electrónico"}
+                                            "Debes ingresar tu correo electrónico"}
                                         {errors.email?.type === "pattern" &&
-                                            "El correo electrónico no es válido"}
+                                            "El correo electrónico ingresado no es válido"}
                                         {errors.email?.type === "maxLength" &&
-                                            "El correo electrónico no puede ser superior a 191 caracteres"}
+                                            "El correo electrónico no puede exceder los 191 caracteres"}
                                     </Form.Control.Feedback>
                                 </Form.Group>
                                 <Form.Group>
-                                    <Form.Label>Clave <Button onClick={() => switchPassword()} variant="link">{state.passwordShowing ? 'Ocultar' : 'Mostrar'}</Button></Form.Label>
-                                    <Form.Control autoComplete="off" className={errors.password ? 'is-invalid' : ''} ref={register({ required: true })} name="password" type={state.passwordShowing ? 'text' : 'password'}></Form.Control>
+                                    <IconContext.Provider value={state.passwordShowing ? { className: "icon-change" } : { className: "icon" }}>
+                                        <Form.Label><span >Clave</span> {state.passwordShowing ? <BiHide type="button" onClick={() => switchPassword()} title="Ocultar clave" />
+                                            : <BiShowAlt type="button" onClick={() => switchPassword()} title="Mostrar clave" />}</Form.Label>
+                                    </IconContext.Provider>
+                                    <Form.Control autoComplete="off" className={errors.password ? 'is-invalid' : 'password-input'} ref={register({ required: true })} name="password" type={state.passwordShowing ? 'text' : 'password'}></Form.Control>
                                     <Form.Control.Feedback type="invalid">
-                                        {errors.password && "Se requiere una clave de acceso"}
+                                        {errors.password && "Debes ingresar tu clave"}
                                     </Form.Control.Feedback>
+                                    <Link className="link" to="#"><small>Olvidé mi clave</small></Link>
                                 </Form.Group>
-                                <Form.Group>
+                                <Form.Group className="d-flex justify-content-between">
                                     <Button type="submit">
-                                        Ingresar
-                                                                        </Button>
+                                        Iniciar sesión
+                                    </Button>
+                                    <span>¿No estás registrado? <Link className="link" to="/registrarse">Crea una cuenta</Link></span>
                                 </Form.Group>
                             </Form>
                         </Card.Body>
                     </Card>
                 </Col>
             </Row>
-        </Container>
+        </Container >
     );
 }
 
