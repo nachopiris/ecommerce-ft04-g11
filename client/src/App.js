5da5294d73f7db57ab01edb667e52d5543ec326e
alantoledo007 <alantoledo007@gmail.com>
2020-09-17 16:05:11 -0300

    Logged user component

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,19 @@
-import AdminProducts from './components/AdminProducts/AdminProducts';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import UserRegister from './components/UserRegister';
-import AddCategories from './components/AddCategory';
-import Product from './components/Product/Product';
-import Catalogue from './components/Catalogue';
-import HomePage from './components/HomePage';
-import Error404 from './components/Error404'
-import Cart from './components/Cart/Cart'
-import Navbar from './components/Navbar';
-import Orders from './components/Orders'
-import Login from './components/Login';
-import Users from './components/Users'
-import './styles/ecommerce.scss';
-import React from 'react';
+import AdminProducts from "./components/AdminProducts/AdminProducts";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import UserRegister from "./components/UserRegister";
+import AddCategories from "./components/AddCategory";
+import Product from "./components/Product/Product";
+import Catalogue from "./components/Catalogue";
+import HomePage from "./components/HomePage";
+import Error404 from "./components/Error404";
+import Cart from "./components/Cart/Cart";
+import Navbar from "./components/Navbar";
+import Orders from "./components/Orders";
+import Login from "./components/Login";
+import Users from "./components/Users";
+import Account from "./components/Account";
+import "./styles/ecommerce.scss";
+import React from "react";
 
 function App() {
     return (
@@ -25,16 +26,22 @@ function App() {
                 <Route exact path="/productos/:id" component={Product} />
                 <Route exact path="/catalogo" component={Catalogue} />
                 <Route exact path="/carrito" component={Cart} />
+                <Route exact path="/cuenta" component={Account} />
 
                 <Route exact path="/admin/usuarios" component={Users}></Route>
-                <Route exact path="/admin/productos" component={AdminProducts} />
-                <Route exact path="/admin/categorias" component={AddCategories} />
+                <Route
+                    exact
+                    path="/admin/productos"
+                    component={AdminProducts}
+                />
+                <Route
+                    exact
+                    path="/admin/categorias"
+                    component={AddCategories}
+                />
                 <Route exact path="/admin/ordenes" component={Orders} />
 
-
                 <Route path="*" component={Error404} />
-
-
             </Switch>
         </React.Fragment>
     );
