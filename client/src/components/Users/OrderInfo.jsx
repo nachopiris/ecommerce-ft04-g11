72ae48b8468ba9e613d905f8eaa817029f918af6
Caldum <nachocaldumbide@gmail.com>
2020-09-25 11:55:02 -0300

    adds corrections

--- client/src/components/Users/OrderInfo.jsx
+++ client/src/components/Users/OrderInfo.jsx
@@ -35,61 +35,10 @@ export default function OrderInfo({ show, handleClose, order, orderlines, allPro
 
     return (
         <Modal show={show} onHide={handleClose}>
-            <Modal.Header className="border-0 bg-dark2" closeButton>
+            <Modal.Header className="border-0 bg-dark2 pb-0" closeButton>
                 <Modal.Title>Detalles de orden N°{id}</Modal.Title>
             </Modal.Header>
             <Modal.Body className="bg-dark2">
-                <Table className="table table-dark table-sm table-striped table-hover">
-                    <thead>
-                        <tr className="text-center">
-                            <th>Producto</th>
-                            <th>Cantidad</th>
-                            <th>Precio</th>
-                            <th>Subtotal</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {(orderlines && orderlines.length > 0) &&
-                            orderlines.map((orderline, index) => {
-                                if (orderline.orderId === id) {
-                                    totalPrice += orderline.price * orderline.quantity;
-
-                                    return (
-                                        <tr key={index} className="text-center">
-                                            <td className="align-middle">{
-                                                (products && products.length > 0) && products.map(product => {
-                                                    if (product.id === orderline.productId) {
-                                                        console.log(product.image)
-                                                        return (
-                                                            <Row className="d-flex justify-content-between text-left">
-                                                                <Col className="m-0" style={{ maxWidth: "4.4rem" }}>
-                                                                    <img style={{ width: "4rem" }} src={JSON.parse(product.images)[0]} alt={product.name} />
-                                                                </Col>
-                                                                <Col className="m-0 d-flex align-items-center justify-content-center text-center">
-                                                                    <span>{product.name}</span>
-                                                                </Col>
-                                                            </Row>
-                                                        )
-                                                    }
-                                                })
-
-                                            }</td>
-                                            <td className="align-middle">{orderline.quantity}</td>
-                                            <td className="align-middle">${orderline.price}</td>
-                                            <td className="align-middle">${orderline.quantity * orderline.price}</td>
-                                        </tr>
-                                    )
-                                }
-                            })}
-                    </tbody>
-                    <tfoot>
-                        <tr className="text-center">
-                            <th colSpan="2"></th>
-                            <th>Total:</th>
-                            <th>${totalPrice}</th>
-                        </tr>
-                    </tfoot>
-                </Table>
                 <Alert className="m-0 mb-1 p-2 small" variant="danger" show={state.showingAlert}>Atención: cambiar el estado puede afectar el proceso de compra</Alert>
                 <Form.Group>
                     <Form.Label>Estado</Form.Label>
@@ -100,6 +49,38 @@ export default function OrderInfo({ show, handleClose, order, orderlines, allPro
                         <option key="4" value="completed">Completada</option>
                     </Form.Control>
                 </Form.Group>
+                <div className="overflow-auto" style={{ maxHeight: '190px' }}>
+                    <ul>
+                        {(orderlines && orderlines.length > 0) &&
+                            orderlines.map((orderline, index) => {
+                                if (orderline.orderId === id) {
+                                    totalPrice += orderline.price * orderline.quantity;
+                                    return (
+                                        <li key={index} className="my-3 p-2 shadow-sm">
+                                            <div className="d-flex justify-content-between ">
+                                                {products && products.length > 0 &&
+                                                    products.map(product => {
+                                                        if (product.id === orderline.productId) {
+                                                            return (
+                                                                <span>
+                                                                    {product.name}<br />
+                                                                    <span className="text-muted">{orderline.quantity} x ${orderline.price}</span>
+                                                                </span>
+                                                            )
+                                                        }
+                                                    })}
+                                                <b>${(orderline.quantity * orderline.price).toFixed(2)}</b>
+                                            </div>
+                                        </li>
+                                    )
+                                }
+                            })}
+                    </ul>
+                </div>
+                <div className="text-right p-3">
+                    <span>Total</span><br />
+                    <span className="h4"><b>${totalPrice.toFixed(2)}</b></span>
+                </div>
             </Modal.Body>
             <Modal.Footer className="d-flex justify-content-between border-0 bg-dark2">
                 <Button variant="danger" onClick={handleClose}>
