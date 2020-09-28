62e440aa1d237836e71783383e34c005c804e32f
Caldum <nachocaldumbide@gmail.com>
2020-09-28 15:16:25 -0300

    adds admin home

--- client/src/actions/categories.js
+++ client/src/actions/categories.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 import config from '../config';
 
-const GET_CATEGORES = 'GET_CATEGORIES';
+const GET_CATEGORIES = 'GET_CATEGORIES';
 const GET_CATEGORY = 'GET_CATEGORY';
 const CREATE_CATEGORY = 'CREATE_CATEGORY';
 const UPDATE_CATEGORY = 'UPDATE_CATEGORY';
@@ -9,31 +9,28 @@ const DELETE_CATEGORY = 'DELETE_CATEGORY';
 
 const BASE_URI = config.api.base_uri + '/categories';
 
-export function getCategories()
-{
+export function getCategories() {
     return dispatch => {
         return Axios.get(BASE_URI)
-        .then(res => res.data)
-        .then(res => {
-            dispatch({type:GET_CATEGORES, payload: res.data});
-        });
+            .then(res => res.data)
+            .then(res => {
+                dispatch({ type: GET_CATEGORIES, payload: res.data });
+            });
     }
 }
 
-export function getCategory(id)
-{
+export function getCategory(id) {
     return dispatch => {
         return Axios.get(BASE_URI + '/' + id)
-        .then(res => res.data)
-        .then(res => {
-            dispatch({type: GET_CATEGORY, payload: res});
-        });
+            .then(res => res.data)
+            .then(res => {
+                dispatch({ type: GET_CATEGORY, payload: res });
+            });
     }
 }
 
 
-export function createCategory(attributes)
-{
+export function createCategory(attributes) {
     /*
         'attributes' es un objeto que contiene como propiedades los atributos del producto. En el Back-End,
         este objeto se interpreta como 'body'.
@@ -41,30 +38,28 @@ export function createCategory(attributes)
     */
     return dispatch => {
         return Axios.post(BASE_URI, attributes)
-        .then(res => res.data)
-        .then(res => {
-            dispatch({type: CREATE_CATEGORY, payload: res});
-        });
+            .then(res => res.data)
+            .then(res => {
+                dispatch({ type: CREATE_CATEGORY, payload: res });
+            });
     }
 }
 
-export function updateCategory(id, attributes)
-{
+export function updateCategory(id, attributes) {
     return dispatch => {
         return Axios.put(BASE_URI + '/' + id, attributes)
-        .then(res => res.data)
-        .then(res => {
-            dispatch({type: UPDATE_CATEGORY, payload: res});
-        })
+            .then(res => res.data)
+            .then(res => {
+                dispatch({ type: UPDATE_CATEGORY, payload: res });
+            })
     }
 }
 
-export function deleteCategory(id)
-{
+export function deleteCategory(id) {
     return dispatch => {
         return Axios.delete(BASE_URI + '/' + id)
-        .then(() => {
-            dispatch({type: DELETE_CATEGORY, payload: id});
-        })
+            .then(() => {
+                dispatch({ type: DELETE_CATEGORY, payload: id });
+            })
     }
 }
